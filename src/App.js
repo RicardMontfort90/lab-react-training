2996d71f00ad25a3ee6b00ad41b91d98bf73ffc2
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-02-28 15:29:45 +0100

    Iteración 6 | Componente:Rating Done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Greetings from './components/Greetings';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 import './App.css';
 
 function App() {
@@ -73,7 +74,13 @@ function App() {
         />
       </div>
 
-      
+      <Rating>0</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
+      <Rating>3</Rating>
+      <Rating>4</Rating>
+      <Rating>5</Rating>
+
     </div>
   );
 }
